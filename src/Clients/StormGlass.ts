db0850cdefe9df2e36e583d809ddaba407a20821
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-01-20 12:06:06 -0300

    Normalização dos dados

--- src/Clients/StormGlass.ts
+++ src/Clients/StormGlass.ts
@@ -1,6 +1,36 @@
 import { AxiosStatic } from 'axios';
 
-class StormClass {
+interface IStormGlassPointSource {
+    [key: string]: number;
+}
+
+interface IStormGlassPoint {
+    readonly time: string;
+    readonly swellDirection: IStormGlassPointSource
+    readonly swellHeight: IStormGlassPointSource
+    readonly swellPeriod: IStormGlassPointSource
+    readonly waveDirection: IStormGlassPointSource
+    readonly waveHeight: IStormGlassPointSource
+    readonly windDirection: IStormGlassPointSource
+    readonly windSpeed: IStormGlassPointSource
+}
+
+interface IStormGlassForecastResponse {
+    hours: Array<IStormGlassPoint>
+}
+
+interface IForecastPoint {
+    time: string;
+    swellDirection: number
+    swellHeight: number
+    swellPeriod: number
+    waveDirection: number
+    waveHeight: number
+    windDirection: number
+    windSpeed: number
+}
+
+class StormGlass {
     private readonly stormGlassApiParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
     private readonly stormGlassApiSource = 'noaa';
 
@@ -9,12 +39,41 @@ class StormClass {
         protected request: AxiosStatic
     ) { }
 
-    public async fetchPoints(lat: number, long: number): Promise<unknown> {
-        return await this.request.get(`
+    public async fetchPoints(lat: number, long: number): Promise<Array<IForecastPoint>> {
+        const response = await this.request.get<IStormGlassForecastResponse>(`
         https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${long}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}
-        `); 
+        `);
+
+        return this.normalizeResponse(response.data);
+    }
+
+    private normalizeResponse(points: IStormGlassForecastResponse): Array<IForecastPoint> {
+        return points.hours.filter(this.isValidPoint.bind(this))
+            .map((point) => ({
+                swellDirection: point.swellDirection[this.stormGlassApiSource],
+                swellHeight: point.swellHeight[this.stormGlassApiSource],
+                swellPeriod: point.swellPeriod[this.stormGlassApiSource],
+                time: point.time,
+                waveDirection: point.waveDirection[this.stormGlassApiSource],
+                waveHeight: point.waveHeight[this.stormGlassApiSource],
+                windDirection: point.windDirection[this.stormGlassApiSource],
+                windSpeed: point.windSpeed[this.stormGlassApiSource],
+            }))
+    }
+
+    private isValidPoint(point: Partial<IStormGlassPoint>): boolean {        
+        return !!(
+            point.time &&
+            point.swellDirection?.[this.stormGlassApiSource] &&
+            point.swellHeight?.[this.stormGlassApiSource] &&
+            point.swellPeriod?.[this.stormGlassApiSource] &&
+            point.waveDirection?.[this.stormGlassApiSource] &&
+            point.waveHeight?.[this.stormGlassApiSource] &&
+            point.windDirection?.[this.stormGlassApiSource] &&
+            point.windSpeed?.[this.stormGlassApiSource]
+        );
     }
 }
 
-
-export default StormClass;
+export { IStormGlassForecastResponse  };
+export default StormGlass;
--- src/Clients/__test__/StormGlass.test.ts
+++ src/Clients/__test__/StormGlass.test.ts
@@ -8,9 +8,9 @@ jest.mock('axios');
 describe('StormGlass Client', () => {
     it('Should return the normalized forecast from the StormGlass service.', async () => {
         const lat = -33.792726;
-        const long = 151.289824; 
+        const long = 151.289824;
 
-        axios.get = jest.fn().mockResolvedValue(stormGlassWeatherThreeHoursFixtures);
+        axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeatherThreeHoursFixtures });
 
         const stormGlass = new StormGlass(axios);
         const response = await stormGlass.fetchPoints(lat, long);
